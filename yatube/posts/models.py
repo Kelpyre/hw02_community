d2032f23acc3f88476d4cdd36a253abff104c2c9
Дмитрий Филимонов <atrom@list.ru>
2022-01-15 20:58:29 +0300

    UPD: 2nd review problems fixed

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,5 +1,3 @@
-# posts/models.py
-
 from django.contrib.auth import get_user_model
 from django.db import models
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,19 @@
-# posts/views.py
-
+from typing import Union
 from django.shortcuts import render, get_object_or_404
-from typing import Type
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, Http404, HttpResponse
 
 from .models import Post, Group
 
+posts_count: int = 10
 
-def index(request):
+
+def index(request: HttpRequest) -> HttpResponse:
     template: str = 'posts/index.html'
     title: str = 'Последние обновления на сайте'
     description: str = 'Главная страница проекта Yatube'
-    posts_count: str = 10
-    posts: Type[Post] = Post.objects.all()[:posts_count]
-    context: dict[str, str] = {
+    posts: QuerySet[Post] = Post.objects.all()[:posts_count]
+    context: dict[str, Union[str, QuerySet]] = {
         'title': title,
         'description': description,
         'posts': posts
@@ -24,14 +25,16 @@ def index(request):
     )
 
 
-def group_list(request, slug):
+def group_list(request: HttpRequest, slug: str) -> HttpResponse:
     template: str = 'posts/group_list.html'
     title: str = f'Записи сообщества {slug}'
-    posts_count: str = 10
-    group_name: Type[Group] = get_object_or_404(Group, slug=slug)
-    posts: Type[Post] = group_name.posts.all()[:posts_count]
+    group_name: Union[Group.title, Http404] = get_object_or_404(
+        Group,
+        slug=slug
+    )
+    posts: QuerySet[Post] = group_name.posts.all()[:posts_count]
     description: str = group_name.description
-    context: dict[str, str] = {
+    context: dict[str, Union[str, QuerySet[Post], Group.title]] = {
         'title': title,
         'description': description,
         'group_name': group_name,
